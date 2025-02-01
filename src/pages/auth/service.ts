2a93c07d6c2f8388a8d0171d634617ab21491f49
Alejandro Hervás <alesepto15@gmail.com>
2025-02-01 23:40:17 +0100

    Created types and logic for login and logout.

---
+++ src/pages/auth/service.ts
@@ -0,0 +1,19 @@
+import {
+  client,
+  removeAuthorizationHeader,
+  setAuthorizationHeader,
+} from "../../api/client";
+import storage from "../../utils/storage";
+import { Credentials, Login } from "./types";
+
+export const login = async (credentials: Credentials) => {
+  const response = await client.post<Login>("/auth/login", credentials);
+  const { accesToken } = response.data;
+  storage.set("auth", accesToken);
+  setAuthorizationHeader(accesToken);
+};
+
+export const logout = async () => {
+  storage.remove("auth");
+  removeAuthorizationHeader();
+};
