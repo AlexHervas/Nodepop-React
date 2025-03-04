39aa634244dd4107333ceb0545844c58fdeb862c
Alejandro Hervás <alesepto15@gmail.com>
2025-03-04 20:53:13 +0100

    Fixed storage, when 'save credentials' is enabled the token is saved in localStorage.

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,33 +1,33 @@
 type StorageKey = "auth"; // Usamos la clave "auth" para el token de acceso
 
-// Objeto que gestiona el almacenamiento de datos en localStorage y sessionStorage
+// Objeto que gestiona el almacenamiento de datos en localStorage
 const storage = {
-  // Método para obtener un valor del almacenamiento, primero intenta en localStorage, luego en sessionStorage
+  // Método para obtener un valor del almacenamiento
   get(key: StorageKey) {
-    return localStorage.getItem(key) || sessionStorage.getItem(key);
+    const value = localStorage.getItem(key);
+
+    if (!value) {
+      return null;
+    }
+    return value;
   },
 
-  // Método para establecer un valor en el almacenamiento (localStorage o sessionStorage) según 'persist'
+  // Método para establecer un valor en el almacenamiento según 'persist'
   set(key: StorageKey, value: string, persist: boolean) {
     // Si 'persist' es true, guarda el valor en localStorage (persistente)
     if (persist) {
       localStorage.setItem(key, value);
-    } else {
-      // Si 'persist' es false, guarda el valor en sessionStorage (solo por la duración de la sesión)
-      sessionStorage.setItem(key, value);
     }
   },
 
   // Método para eliminar un valor del almacenamiento
   remove(key: StorageKey) {
     localStorage.removeItem(key);
-    sessionStorage.removeItem(key);
   },
 
-  // Método para limpiar todo el almacenamiento (localStorage y sessionStorage)
+  // Método para limpiar todo el almacenamiento
   clear() {
     localStorage.clear();
-    sessionStorage.clear();
   },
 };
 
